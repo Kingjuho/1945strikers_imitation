5c27fedc9da66d1241918817dc26b8014b6f672d
Jhlee <ljh990627@gmail.com>
2026-01-21 15:50:24 +0900

    주석 삭제

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,13 +5,11 @@ public class Background : MonoBehaviour
     public float scrollSpeed = 0.01f;
     Material bgMaterial;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         bgMaterial = GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
     void Update()
     {
         // 오프셋 조절을 통한 배경 흐르는 효과 적용
