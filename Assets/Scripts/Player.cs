b82214ba94a7d6a9a2ed643ce02ba5f2a8159aa9
Jhlee <ljh990627@gmail.com>
2026-01-21 15:50:16 +0900

    플레이어 구현
    1. bpm -> png 변환
    2. 기본 재생 애니메이션 삽입
    3. 방향키로 움직이도록 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 방향 * 시간 * 스피드
+        float distanceX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
+        float distanceY = Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(distanceX, distanceY, 0);
+    }
+}
