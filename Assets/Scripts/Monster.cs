026aa8020532b821fe18a2a9701e90abbe09e833
Jhlee <ljh990627@gmail.com>
2026-01-22 14:45:33 +0900

    적 총알 발사 기능 구현
    1. 적 총알 애니메이션 추가
    2. 총알과 적 속도 조정(총알 5, 적 2)

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] int hp = 100;
+    [SerializeField] float speed = 2f;
+    [SerializeField] float delay = 1f;
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+
+    public GameObject bullet;
+
+    void Start()
+    {
+        InvokeRepeating("Shoot", delay, delay);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+
+    // 카메라 밖으로 나갈 경우 삭제
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // 총알 발사 함수
+    void Shoot()
+    {
+        Instantiate(bullet, pos1.position, Quaternion.identity);
+        Instantiate(bullet, pos2.position, Quaternion.identity);
+    }    
+
+    // 데미지를 받는 함수
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0) Dead();
+    }
+
+    // 사망 처리 함수
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/MonsterBullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MonsterBullet : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float attack;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 아래쪽 방향으로 발사
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+
+    // 카메라 밖으로 나갈 경우 삭제
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // isTrigger가 체크된 객체와 충돌 시 호출되는 함수
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+        }
+
+        Destroy(gameObject);
+    }
+}
