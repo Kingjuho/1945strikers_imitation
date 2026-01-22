110e6752018d890915583c91f2d8b2f706c773ca
Jhlee <ljh990627@gmail.com>
2026-01-22 11:16:30 +0900

    플레이어 총알 프리팹 추가

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField] float speed = 4.0f;
+    [SerializeField] float attack;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 위쪽 방향으로 발사
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    // 카메라 밖으로 나갈 경우 삭제
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
