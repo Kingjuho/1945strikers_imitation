217870ce2493a3f4e8223f8c172e7e526238e146
Jhlee <ljh990627@gmail.com>
2026-01-26 09:40:31 +0900

    스폰 기능 추가
    DeadEffect를 빨리 추가해야 함

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] float rowStart = -2f;
+    [SerializeField] float rowEnd = 2f;
+    [SerializeField] float startTime = 1;
+    [SerializeField] float stopSpawn = 10;
+
+    [SerializeField] GameObject monster1;
+    [SerializeField] GameObject monster2;
+
+    bool _swi = true;
+
+    void Start()
+    {
+        StartCoroutine("RandomSpawn");
+        Invoke("Stop", stopSpawn);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 랜덤 스폰 코루틴
+    IEnumerator RandomSpawn()
+    {
+        while(_swi)
+        {
+            // 1초 마다
+            yield return new WaitForSeconds(startTime);
+            
+            // x, y 위치 선정
+            float x = Random.Range(rowStart, rowEnd);
+            float y = transform.position.y;
+            Vector2 xy = new Vector2(x, y);
+
+            // 생성
+            Instantiate(monster1, xy, Quaternion.identity);
+        }
+    }
+
+    // 스폰 중지
+    void StopSpawn()
+    {
+        _swi = false;
+        StopCoroutine("RandomSpawn");
+    }
+}
