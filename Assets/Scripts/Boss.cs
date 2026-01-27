160cd14925455a0b2638b7b7e1acb102aeaa38c2
Jhlee <ljh990627@gmail.com>
2026-01-27 10:10:11 +0900

    보스 스크립트 구현, 태그 추가

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    int flag = 1;
+    int speed = 2;
+
+    public GameObject mb;
+    public GameObject mb2;
+    public Transform pos1;
+    public Transform pos2;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        Invoke("Hide", 2);
+        StartCoroutine("BossMissile");
+    }
+
+    void Hide() { GameObject.Find("TextBossWarning").SetActive(false); }
+
+    IEnumerator BossMissile()
+    {
+        while(true)
+        {
+            Instantiate(mb, pos1.position, Quaternion.identity);
+            Instantiate(mb, pos2.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
